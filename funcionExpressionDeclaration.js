8451a8d35ebaa179bb322e0f47c9d230d1465dcc
raydabbah <dabbahray@yahoo.com>
2017-07-06 22:49:24 -0400

    added function exxpression and declaration

---
+++ funcionExpressionDeclaration.js
@@ -0,0 +1,12 @@
+var expression = function(){
+    return 'expression value ';
+}()
+
+function declaration(){
+    return 'declaration value';
+}
+declaration()
+console.log(expression)
+declaration()
+console.log( expression)
+console.log ( declaration())
